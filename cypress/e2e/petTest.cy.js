5db3e6872f304c135143d3f6bb49b6a5d33b5077
Svitlana Chorna <sv.schwarz1@gmail.com>
2023-06-12 22:30:01 +0300

    update with form data and delete are added

--- cypress/e2e/petTest.cy.js
+++ cypress/e2e/petTest.cy.js
@@ -63,8 +63,82 @@ it('Find pet by status', () => {
       return myPet.id === pet.id
     })
 
-    // console.log(resultPetArray);
+    console.log(resultPetArray);
 
     expect(resultPetArray[0]).to.be.eql(pet);
   })
 })
+
+it.only('Update pet with form data', () => {
+  const updatedPet = {
+    id: 2537295756394496,
+    name: "Jane",
+    status: 'available'
+  };
+
+  cy.log(`Update pet with id: ${updatedPet.id} using form data`);
+  
+  cy.request ({
+    method: 'POST', 
+    url: `/pet/${updatedPet.id}`,
+    form: true, 
+    body: {
+      id: updatedPet.id,
+      name: updatedPet.name,
+      status: updatedPet.status,
+    }
+  }).then( response => {
+    expect(response.status).to.be.equal(200);
+    expect(response.body.message).to.be.equal(updatedPet.id.toString());
+  })
+
+  cy.request ({
+    method: 'GET', 
+    url: `/pet/${updatedPet.id}`,
+    body: {
+      id: updatedPet.id,
+      name: updatedPet.name,
+      status: updatedPet.status,
+    }
+  }).then( response => {
+    cy.log(response); 
+    expect(response.status).to.be.equal(200);
+    expect(response.body.id).to.be.equal(updatedPet.id);
+    expect(response.body.name).to.be.equal(updatedPet.name);
+    expect(response.body.status).to.be.equal(updatedPet.status);
+  })
+})
+
+it.only('Delete pet', () => {
+  const petToDelete = {
+    id: 0
+  };
+  cy.log(`Delete pet with id ${petToDelete.id}`)
+
+  cy.request ({
+    failOnStatusCode: false,
+    method: 'DELETE', 
+    url: `/pet/${petToDelete.id}`,
+    body: {
+      id: petToDelete.id,
+    }
+  }).then( response => {
+    expect(response.status).to.be.equal(404);
+  })
+
+  cy.request ({
+    failOnStatusCode: false,
+    method: 'GET', 
+    url: `/pet/${petToDelete.id}`,
+    body: {
+      id: petToDelete.id,
+    }
+  }).then( response => {
+    cy.log(response); 
+    expect(response.status).to.be.equal(404);
+    expect(response.body.code).to.be.equal(1);
+    expect(response.body.type).to.be.equal('error');
+    expect(response.body.message).to.be.equal('Pet not found');
+  })
+})
+
